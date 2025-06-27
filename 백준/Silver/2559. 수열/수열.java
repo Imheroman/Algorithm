0b75caf6e64d2abed5d67841ec4ae02e86ad3a32
김영웅 <101276511+Imheroman@users.noreply.github.com>
2025-06-28 01:15:40 +0900

    [Silver III] Title: 수열, Time: 244 ms, Memory: 24088 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -1,7 +1,5 @@
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -10,16 +8,18 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        List<Integer> numbers = new ArrayList<>();
-        new StringTokenizer(br.readLine()).asIterator().forEachRemaining(val -> numbers.add(Integer.parseInt((String) val)));
+        st = new StringTokenizer(br.readLine());
 
-        int answer = Integer.MIN_VALUE;
-        for (int i = 0; i < n - k + 1; i++) {
-            int result = 0;
-            for (int j = i; j < i + k; j++) {
-                result += numbers.get(j);
-            }
-            answer = Math.max(answer, result);
+        int[] repository = new int[n];
+
+        repository[0] = Integer.parseInt(st.nextToken());
+        for (int i = 1; i < n; i++) {
+            repository[i] = repository[i - 1] + Integer.parseInt(st.nextToken());
+        }
+
+        int answer = repository[k - 1];
+        for (int i = k; i < n; i++) {
+            answer = Math.max(answer, repository[i] - repository[i - k]);
         }
 
         System.out.println(answer);
