aba951c5163ac573cc46d44b8320779a35c2f25c
김영웅 <101276511+Imheroman@users.noreply.github.com>
2025-07-26 01:17:09 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 556 ms, Memory: 51808 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -10,24 +10,17 @@ public class Main {
 		int N = getInt(st), M = getInt(st);
 		
 		st = input();
-		int[] numbers = new int[N];
-		numbers[0] = getInt(st);
+		int[] numbers = new int[N + 1];
 		
-		for (int i = 1; i < N; i++) {
+		for (int i = 1; i <= N; i++) {
 			numbers[i] = getInt(st) + numbers[i - 1];
 		}
 		
 		StringBuilder answer = new StringBuilder(); 
 		for (int t = 0; t < M; t++) {
 			st = input();
-			int start = getInt(st) - 1, end = getInt(st) - 1;
-			
-			if (start == 0) {
-				answer.append(numbers[end]);
-			} else {
-				answer.append(numbers[end] - numbers[start - 1]);
-			}
-			answer.append("\n");
+			int start = getInt(st), end = getInt(st);
+			answer.append(numbers[end] - numbers[start - 1]).append("\n");
 		}
 		
 		System.out.println(answer);
