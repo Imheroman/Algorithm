327aab8108b8450c2d4259909d17989240c452b4
김영웅 <101276511+Imheroman@users.noreply.github.com>
2025-08-21 00:31:57 +0900

    [Bronze I] Title: 도비의 난독증 테스트, Time: 112 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Bronze/2204. 도비의 난독증 테스트/도비의 난독증 테스트.java
+++ 백준/Bronze/2204. 도비의 난독증 테스트/도비의 난독증 테스트.java
@@ -7,18 +7,22 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		int n;
 
+		StringBuilder answer = new StringBuilder();
+		List<String> list = new ArrayList();
 		while ((n = getInt()) > 0) {
-			List<String> list = new ArrayList();
 			list.sort(String::compareToIgnoreCase);
 			for (int i = 0; i < n; i++) {
 				list.add(br.readLine());
 			}
 			
 			list.sort(String::compareToIgnoreCase);
-			System.out.println(list.get(0));
+			answer.append(list.get(0)).append("\n");
+			list.clear();
 		}
+		
+		System.out.println(answer);
 	}
-	
+
 	public static int getInt() throws IOException {
 		return Integer.parseInt(br.readLine());
 	}
