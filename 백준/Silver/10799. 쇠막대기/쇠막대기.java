73975079cfe6b71d01bb3ef5f718a6dfdea3fb05
김영웅 <101276511+Imheroman@users.noreply.github.com>
2025-08-23 14:39:24 +0900

    [Silver II] Title: 쇠막대기, Time: 132 ms, Memory: 15744 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static char[] letters;
+
+	public static void main(String[] args) throws IOException {
+		init();
+		solve();
+	}
+	
+	public static void init() throws IOException{
+		letters = new BufferedReader(new InputStreamReader(System.in)).readLine().toCharArray();
+	}
+	
+	public static void solve() {
+		int cnt = 0, ans = 0;
+		
+		for (int i = 1; i < letters.length; i++) {
+			if (letters[i] == '(') ++cnt;
+			else if (letters[i - 1] == '(' && letters[i] == ')') ans += cnt--;
+			else if(letters[i - 1] == ')' && letters[i] == ')') {
+				--cnt;
+				++ans;
+			}
+		}
+		
+		System.out.println(ans);
+	}
+}
