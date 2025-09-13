f1b8b3e32b9fbfa8379ba5d4697e0c477695d52c
김영웅 <101276511+Imheroman@users.noreply.github.com>
2025-09-14 02:28:09 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1912 ms, Memory: 351268 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -4,13 +4,14 @@ import java.util.*;
 public class Main {
 	static BufferedReader br;
 	static int N, M, ans;
-	static Edge[] edges;
-	static int[] parents;
+	static List<Node>[] nodes;
+	static Node startNode;
 	
 	public static void main(String[] args) throws IOException {
 		br = new BufferedReader(new InputStreamReader(System.in));
+		
 		init();
-		solve();
+		prim();
 		System.out.println(ans);			
 	}
 	
@@ -22,64 +23,57 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		edges = new Edge[M];
-		parents = new int[N + 1];
-		
-		for (int i = 0; i < N + 1; i++) parents[i] = i;
+		nodes = new List[N + 1];
+		for (int i = 0; i < N + 1; i++) nodes[i] = new ArrayList<>();
 		
 		for (int m = 0; m < M; m++) {
 			st = new StringTokenizer(br.readLine());
+			
 			int from = Integer.parseInt(st.nextToken()),
 					to = Integer.parseInt(st.nextToken()),
-					w = Integer.parseInt(st.nextToken());
-			
-			edges[m] = new Edge(from, to, w);
+					weight = Integer.parseInt(st.nextToken());
+
+            // 양방향으로 이어주기
+			nodes[from].add(new Node(to, weight));
+			nodes[to].add(new Node(from, weight));
 		}
-		
-		Arrays.sort(edges);
 	}
 	
-	static void solve() {
-		int cnt = 0;
+	static void prim() {
+		Queue<Node> queue = new PriorityQueue<Main.Node>();
+		boolean[] visited = new boolean[N + 1];
 		
-		for (Edge e : edges) {
-			if (cnt == N - 2) break;  // 간선의 수이기 때문에 N - 1
-			if (!union(e.from, e.to)) continue;  // 이미 서로 같은 조상이라면 Pass
+		queue.offer(new Node(1, 0)); // 임의 정점 삽입 (시작 정점이므로 cost == 0)
+		
+		int max = 0;  // 최댓값 저장
+		while (!queue.isEmpty()) {  // 큐에 데이터가 있으면 계속
+			Node node = queue.poll();  // 가장 최소 거리 가져오기
 			
-			++cnt;
-			ans += e.weight;
+			if (visited[node.to]) continue;  // 방문한 적 있는 노드면 pass
+			
+			visited[node.to] = true;  // 방문 처리
+			ans += node.weight;  // 거리 누적
+			if (max < node.weight) max = node.weight;  // max값 더하기
+			
+			for (Node n : nodes[node.to]) { // 현재 방문 노드에서 갈 수 있는 모든 노드들에 대해
+				if (visited[n.to]) continue;  // 방문했으면 pass
+				queue.offer(n);  // 방문 안 했으면 일단 다 넣기 (최솟값을 PQ로 구하기 때문에)
+			}
 		}
+      
+		ans -= max;  // 최댓값 빼주기
 	}	
 	
-	static int find(int cur) {  // 최상위 부모를 찾아감 
-		if (parents[cur] == cur) return cur;  // 내가 최상위 부모면 return
-		return parents[cur] = find(parents[cur]);  // 계속 찾아감 
-	}
-	
-	static boolean union(int a, int b) {
-		int aroot = find(a);
-		int broot = find(b);
-		
-		if (aroot == broot) return false;  // 부모가 같은 경우에는 union 불가
-		
-        // 더 작은 쪽으로 붙여주기
-		if (aroot > broot) parents[aroot] = broot;
-		else parents[broot] = aroot;
-		
-		return true;
-	}
-	
-	static class Edge implements Comparable<Edge>{
-		int from, to, weight;
+	static class Node implements Comparable<Node>{
+		int to, weight;
 
-		public Edge(int from, int to, int weight) {
-			this.from = from;
+		public Node(int to, int weight) {
 			this.to = to;
 			this.weight = weight;
 		}
 
 		@Override
-		public int compareTo(Main.Edge o) {
+		public int compareTo(Main.Node o) {
 			return Integer.compare(this.weight, o.weight);
 		}
 	}
