f31595581a9c9fe959e30be2e3b54b958ae3eddc
김영웅 <101276511+Imheroman@users.noreply.github.com>
2025-10-03 21:00:03 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 608 ms, Memory: 61096 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -13,24 +13,22 @@ public class Main {
 
 	static void init() throws IOException {
 		sb = new StringBuilder();
+		Set<int[]> numbers = new TreeSet<>((pre, post) -> pre[1] != post[1] ? 
+				Integer.compare(pre[1], post[1]) : Integer.compare(pre[0], post[0]));
 		int n = Integer.parseInt(br.readLine());
-		int[][] numbers = new int[n][];
 		
 		StringTokenizer st;
 		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
 			int x = Integer.parseInt(st.nextToken()),
 					y = Integer.parseInt(st.nextToken());
-			numbers[i] = new int[] {x, y};
+			numbers.add(new int[] {x, y});
 		}
 		
-		solve(numbers, n);
+		solve(numbers);
 	}
 	
-	static void solve(int[][] numbers, int n) {
-		Arrays.sort(numbers, (pre, post) -> pre[1] != post[1] ? 
-				Integer.compare(pre[1], post[1]) : Integer.compare(pre[0], post[0]));
-		
+	static void solve(Set<int[]> numbers) {
 		for (int[] number : numbers) 
 			sb.append(number[0]).append(" ").append(number[1]).append("\n");
 		
