aad83c247573af41b8436f57c3b07431a47bf3ee
김영웅 <101276511+Imheroman@users.noreply.github.com>
2025-10-04 15:00:50 +0900

    [Silver V] Title: 배열 합치기, Time: 1220 ms, Memory: 343612 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.java
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class Main {
 	static BufferedReader br;
 	static StringBuilder sb;
+	static int[] nNumbers, mNumbers, numbers;
 	
 	public static void main(String[] args) throws IOException {
 		br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,25 +13,33 @@ public class Main {
 
 	static void init() throws IOException {
 		sb = new StringBuilder();
-		Queue<Integer> numbers = new PriorityQueue<>(Comparator.naturalOrder()); 
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
 		
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < n; i++) 
-			numbers.offer(Integer.parseInt(st.nextToken()));
+		nNumbers = new int[n];
+		mNumbers = new int[m];
+		numbers = new int[n + m];
 		
 		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < m; i++) 
-			numbers.offer(Integer.parseInt(st.nextToken()));
+		for (int i = 0; i < n; i++) nNumbers[i] = Integer.parseInt(st.nextToken());
 		
-		solve(numbers);
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < m; i++) mNumbers[i] = Integer.parseInt(st.nextToken());
+		solve();
 	}
 	
-	static void solve(Queue<Integer> numbers) {
-		while (!numbers.isEmpty()) sb.append(numbers.poll()).append(" ");
+	static void solve() {
+		int n = 0, m = 0, i = 0;
 		
+		while (n < nNumbers.length && m < mNumbers.length) 
+			numbers[i++] = nNumbers[n] < mNumbers[m] ? 
+					nNumbers[n++] : mNumbers[m++];
+		
+		while (n < nNumbers.length) numbers[i++] = nNumbers[n++];
+		while (m < mNumbers.length) numbers[i++] = mNumbers[m++];
+			
+		for (int number : numbers) sb.append(number).append(" ");
 		System.out.println(sb);
 	}
 }
