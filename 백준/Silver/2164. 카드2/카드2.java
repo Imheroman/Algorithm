bb0f3e5e56445f9a4c68fb435495346cde4eca19
김영웅 <101276511+Imheroman@users.noreply.github.com>
2025-10-15 00:35:08 +0900

    [Silver IV] Title: 카드2, Time: 88 ms, Memory: 15560 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,24 +1,26 @@
 import java.io.*;
 import java.util.*;
 
+// 참고 블로그: https://st-lab.tistory.com/188
 public class Main {
-	static StringBuilder sb = new StringBuilder();
-	
 	public static void main(String[] args) throws IOException {
-		ArrayDeque<Integer> queue = new ArrayDeque<>();
-        
 		int N = Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine());
-		for (int i = 1; i < N + 1; i++) queue.offer(i);
-        
-		solve(queue);
+		
+		int[] queue = new int[2 * N];	  
+		for(int i = 0; i < N; i++) queue[i] = i +1;
+		int head = 0;  // queue의 head
+		int tail = N - 1;  // queue의 tail
+		
+		solve(queue, N, head, tail);
 	}
 
-	static void solve(ArrayDeque<Integer> queue) {
-		while (queue.size() > 1) {
-			queue.poll();
-			queue.offerLast(queue.poll());
+	static void solve(int[] queue, int N, int head, int tail) {
+		while (N-- > 1) {  // 데이터가 1개 남을 때 까
+//			System.out.println("N: " + N + ", head: " + head + ", tail: " + tail + " -> queue: " + Arrays.toString(queue));
+			queue[++tail] = queue[++head];  // tail의 다음에 처음 버리고 값 넣 
+			++head;  // 값 버리기
 		}
 		
-		System.out.println(queue.poll());
+		System.out.println(queue[head]);
 	}
 }
